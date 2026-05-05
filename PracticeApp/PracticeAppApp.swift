d898f19088549e0b3a7f945fb9c5cd8e80336e71
7wsxb <19712766185@163.com>
2026-05-05 16:40:23 +0800

    Initial Commit

---
+++ PracticeApp/PracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAppApp.swift
+//  PracticeApp
+//
+//  Created by Wang on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
